4a2692150ef43a90e0e405410b17c196b63f0711
solaleee <sola4114@naver.com>
2022-04-01 23:59:52 +0900

    회원가입(join) 및 로그인(login) 서버

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+import jwt
+
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
